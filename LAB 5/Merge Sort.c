54f9c165c589bb4e32ae92417afac0db63d0880b
Shraddha-357 <shraddha.cs22@bmsce.ac.in>
2024-07-17 13:25:35 +0530

    Add files via upload

---
+++ LAB 5/Merge Sort.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+void split(int a[], int low, int high);
+void combine(int a[], int low, int mid, int high);
+
+void split(int a[], int low, int high) {
+    int mid;
+    if (low < high) {
+        mid = (low + high) / 2;
+        split(a, low, mid);
+        split(a, mid + 1, high);
+        combine(a, low, mid, high);
+    }
+}
+
+void combine(int a[], int low, int mid, int high) {
+    int c[15000];
+    int i = low, j = mid + 1, k = low;
+
+    while (i <= mid && j <= high) {
+        if (a[i] < a[j]) {
+            c[k++] = a[i++];
+        } else {
+            c[k++] = a[j++];
+        }
+    }
+
+    while (i <= mid) {
+        c[k++] = a[i++];
+    }
+
+    while (j <= high) {
+        c[k++] = a[j++];
+    }
+
+    for (i = low; i <= high; i++) {
+        a[i] = c[i];
+    }
+}
+
+int main() {
+    int n, i;
+    int a[1000]; // Adjust size as needed
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    split(a, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
